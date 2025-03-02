c58d30b0dccea3356481d8bb479b8b86a286b36c
David Velasco <davvelsan@gmail.com>
2025-03-02 01:10:31 +0100

    extract inverse-lerp and remap functions

--- src/lessons/05-1-sin-cos/shader.frag.glsl
+++ src/lessons/05-1-sin-cos/shader.frag.glsl
@@ -6,14 +6,7 @@ varying vec2 vUvs;
 vec3 red = vec3(1.0, 0.0, 0.0);
 vec3 blue = vec3(0.0, 0.0, 1.0);
 
-float inverseLerp(float v, float minValue, float maxValue) {
-  return (v - minValue) / (maxValue - minValue);
-}
-
-float remap(float v, float inMin, float inMax, float outMin, float outMax) {
-  float t = inverseLerp(v, inMin, inMax);
-  return mix(outMin, outMax, t);
-}
+#pragma glslify: remap = require(../../shared/remap.glsl)
 
 void main() {
   vec3 color = vec3(0.0);
---
+++ src/shared/remap.glsl
@@ -0,0 +1,12 @@
+#pragma glslify: inverseLerp = require(./inverse_lerp.glsl)
+
+/**
+ * Linearly interpolate a value from one range to another.
+ * https://docs.unity3d.com/Packages/com.unity.shadergraph@17.2/manual/Remap-Node.html
+ */
+float remap(float v, float inMin, float inMax, float outMin, float outMax) {
+  float t = inverseLerp(v, inMin, inMax);
+  return mix(outMin, outMax, t);
+}
+
+#pragma glslify: export(remap)

75f0c4ab26055cba402b0e777d2705af5dfa5944
David Velasco <davvelsan@gmail.com>
2025-03-17 23:58:27 +0100

    sdf transformations

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { createExperience } from './lessons/08-1-sdfs';
+import { createExperience } from './lessons/08-3-transformations';
 
 import '@helpers/styles/reset.css';
 import '@helpers/styles/webgl.css';

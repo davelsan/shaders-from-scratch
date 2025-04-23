18babf72348441c653eef94e9b25590d81eff2fa
David Velasco <davvelsan@gmail.com>
2025-04-23 11:02:09 +0200

    Allow setting a maximum device pixel ratio

--- src/helpers/atoms/atomWithViewport.ts
+++ src/helpers/atoms/atomWithViewport.ts
@@ -12,23 +12,30 @@ export type ViewportAtomValue = {
 
 export type ViewportAtom = ReturnType<typeof atomWithViewport>;
 export type ViewportOptions = {
+  /**
+   * Maximum device pixel ratio to use when rendering the scene. This value can
+   * be also changed using the property setter of the return object.
+   * Defaults to `2`.
+   */
   maxPixelRatio?: number;
 };
 
 export const atomWithViewport = (
   store: Store,
   selector: string,
-  options?: ViewportOptions
+  { maxPixelRatio = 2 }: ViewportOptions = {}
 ) => {
+  let _maxPixelRatio = maxPixelRatio;
+
   const el = document.querySelector<HTMLElement>(selector);
   if (!el) {
     throw new Error(`Element with selector "${selector}" not found`);
   }
 
-  const vpAtom = atom(getVp(el, options));
+  const vpAtom = atom(getVp(el, _maxPixelRatio));
 
   vpAtom.onMount = (set) => {
-    const unsub = onWindowResize(() => set(getVp(el, options)), true);
+    const unsub = onWindowResize(() => set(getVp(el, _maxPixelRatio)), true);
     return unsub;
   };
 
@@ -49,20 +56,25 @@ export const atomWithViewport = (
     get pixelRatio() {
       return store.get(vpAtom).pixelRatio;
     },
+    /**
+     * Write-only property to set the maximum device pixel ratio used when
+     * rendering the scene.
+     */
+    set maxPixelRatio(value: number) {
+      _maxPixelRatio = value;
+      store.set(vpAtom, getVp(el, value));
+    },
     sub(...args: SubscribeToAtomArgs<ViewportAtomValue, void>) {
       return subscribe(store, vpAtom, ...args);
     },
   };
 };
 
-function getVp(el: HTMLElement, options?: ViewportOptions) {
+function getVp(el: HTMLElement, maxPixelRatio: number) {
   const width = el.clientWidth;
   const height = el.clientHeight;
   const aspectRatio = width / height;
-  const pixelRatio = Math.min(
-    options?.maxPixelRatio ?? 2,
-    window.devicePixelRatio
-  );
+  const pixelRatio = Math.min(maxPixelRatio, window.devicePixelRatio);
   return {
     root: el,
     width,
